5dc36a6fe65bfc498bdd64b36c36c07ff90e2acc
vtoper <vincent.toper@gmail.com>
2022-08-21 22:17:28 +0200

    G_StoneCarver

--- modules/php/Buildings/G/G_StoneCarver.php
+++ modules/php/Buildings/G/G_StoneCarver.php
@@ -1,6 +1,8 @@
 <?php
 namespace CAV\Buildings\G;
 
+use CAV\Helpers\Utils;
+
 class G_StoneCarver extends \CAV\Models\Building
 {
   public function __construct($row)
@@ -9,7 +11,10 @@ class G_StoneCarver extends \CAV\Models\Building
     $this->type = 'G_StoneCarver';
     $this->category = 'material';
     $this->name = clienttranslate('Stone Carver');
-    $this->desc = [clienttranslate('immediately'), clienttranslate('every time you furnish a cavern or build a stable, you receive a discount of')];
+    $this->desc = [
+      clienttranslate('immediately'),
+      clienttranslate('every time you furnish a cavern or build a stable, you receive a discount of'),
+    ];
     $this->tooltip = [
       clienttranslate(
         'When building the Stone carver, immediately (and only once) get 2 Stone from the general supply.'
@@ -19,5 +24,20 @@ class G_StoneCarver extends \CAV\Models\Building
     ];
     $this->cost = [WOOD => 1];
     $this->vp = 1;
+  }
+
+  protected function onBuy($player, $eventData)
+  {
+    return $this->gainNode([STONE => 2]);
+  }
+
+  public function onPlayerComputeCostsFurnish($player, &$args)
+  {
+    Utils::addBonus($args['costs'], [STONE => -1], $this->id);
+  }
+
+  public function onPlayerComputeCostsStables($player, &$args)
+  {
+    Utils::addBonus($args['costs'], [STONE => -1], $this->id);
   }
 }
