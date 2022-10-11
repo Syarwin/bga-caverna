9f1db30552bb85aff5ac348b30d5576949b58d19
Syarwin <timothee.pecatta@ens-lyon.fr>
2022-10-11 13:24:26 +0200

    73484: "Cost Fodder Chamber incorrect"

--- modules/php/Buildings/Y/Y_FodderChamber.php
+++ modules/php/Buildings/Y/Y_FodderChamber.php
@@ -18,7 +18,7 @@ class Y_FodderChamber extends \CAV\Models\Building
         '__(For instance, you will get 1/2/3/… Bonus points for 3-5/6-8/9-11/… Farm animals, respectively. Dogs are not considered Farm animals. You will get the usual points for “Farm animals and Dogs” regardless.)__'
       ),
     ];
-    $this->cost = [STONE => 1, GRAIN => 1];
+    $this->cost = [STONE => 1, GRAIN => 2];
     $this->beginner = true;
   }
 
